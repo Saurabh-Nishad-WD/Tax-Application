b6931b33b906500935a71ac87325a90de171df9e
saurabh Nishad <saurabhnishad.7651@gmail.com>
2025-01-24 02:11:40 +0530

    secure the leaked important informatin and store in .env file and add it to .gitignore

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -2,15 +2,14 @@ import {createContext, useContext} from 'react';
 import {initializeApp} from 'firebase/app';
 // import {getAuth,createUserWithEmailAndPassword} from "firebase/auth";
 import {getDatabase,set,ref} from 'firebase/database'
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCSaBx5kjUgEY-5ikXfYOWDhIynZef3x4M",
-    authDomain: "tax-application-a44db.firebaseapp.com",
-    projectId: "tax-application-a44db",
-    storageBucket: "tax-application-a44db.firebasestorage.app",
-    messagingSenderId: "32636362253",
-    appId: "1:32636362253:web:a5c95454801d693130bf44",
-    databaseURL: "https://tax-application-a44db-default-rtdb.firebaseio.com/"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 }; 
 
 const firebaseApp = initializeApp(firebaseConfig);
